ca0c3fdeb547217e864b401ac2700a74f0bd4328
Bigpolshark <if20b278@technikum-wien.at>
2021-12-17 00:12:30 +0100

    init

---
+++ MonsterTradingCardGamePP/Enum/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGamePP.Enum
+{
+    enum CardType
+    {
+        Monster,
+        Spell
+    }    
+    
+    enum Element
+    {
+        Normal,
+        Fire,
+        Water
+    }   
+    
+    enum MonsterType
+    {
+        Goblin,
+        Dragon,
+        Wizzard,
+        Ork,
+        Knight,
+        Kraken,
+        Elf
+    }
+}
---
+++ MonsterTradingCardGamePP/Program.cs
@@ -0,0 +1,22 @@
+﻿using MonsterTradingCardGamePP.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGamePP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardType test = CardType.Spell;
+            Console.WriteLine($"Check {test}");
+
+            //remove later
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
